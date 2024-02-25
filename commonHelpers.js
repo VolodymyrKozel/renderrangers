e9a8313852ee92c8a6abff7b7f02f15a2dd3f14d
VolodymyrKozel <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-25 11:07:43 +0000

    Deploying to gh-pages from @ VolodymyrKozel/renderrangers@34c13a3e9b8611a39f87d0a4ab5667871c97f23e 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,21 +1,21 @@
-import{g as u,r,a as g}from"./assets/renderMarkup-e7327bcc.js";import"./assets/vendor-0cb09735.js";const h=async()=>{const t=await u("category-list");t.unshift({list_name:"ALL CATEGORIES"}),console.log(t)};h();console.log("category-list");const l=document.getElementById("books-list"),k=async()=>{try{const t=await u("top-books"),e=f(t,4);w(e)}catch(t){console.error(t)}finally{}};k();function f(t,e){const o=[];for(;o.length<e;){const n=Math.floor(Math.random()*t.length);o.includes(n)||o.push(n)}return o.map(n=>t[n])}function w(t){const e=y(t);l.innerHTML="",l.appendChild(e)}function y(t){const e=document.createDocumentFragment();return t.forEach(({list_name:o,books:n})=>{const s=document.createElement("li");s.className="sellers-category",s.innerHTML=`
+import{r as n,a as d}from"./assets/renderMarkup-15bca558.js";import{a as h}from"./assets/vendor-0cb09735.js";const k="https://books-backend.p.goit.global/books/";async function u(t,e){try{return(await h.get(k+t,{params:e})).data}catch(o){console.error(o)}}const f=async()=>{const t=await u("category-list");t.unshift({list_name:"ALL CATEGORIES"}),console.log(t)};f();console.log("category-list");const l=document.getElementById("books-list"),w=async()=>{try{const t=await u("top-books"),e=y(t,4);b(e)}catch(t){console.error(t)}finally{}};w();function y(t,e){const o=[];for(;o.length<e;){const r=Math.floor(Math.random()*t.length);o.includes(r)||o.push(r)}return o.map(r=>t[r])}function b(t){const e=L(t);l.innerHTML="",l.appendChild(e)}function L(t){const e=document.createDocumentFragment();return t.forEach(({list_name:o,books:r})=>{const s=document.createElement("li");s.className="sellers-category",s.innerHTML=`
       <h2 class="sellers-category-title">${o}</h2>
       <ul class="sellers-category-list">
-        ${L(n)}
+        ${v(r)}
       </ul>
       <button class="sellers-button" type="button">see more</button>
-    `,e.appendChild(s)}),e}function L(t){let e="";const o=b(),n=o==="desktop"?5:o==="tablet"?3:1;for(let s=0;s<n&&s<t.length;s++){const{book_image:d,title:i,author:m,_id:p}=t[s];e+=`
+    `,e.appendChild(s)}),e}function v(t){let e="";const o=I(),r=o==="desktop"?5:o==="tablet"?3:1;for(let s=0;s<r&&s<t.length;s++){const{book_image:m,title:i,author:g,_id:p}=t[s];e+=`
       <li class="sellers-item" data-id="${p}">
         <div class="book-image-wrapper">
           <img class="book-image"
-          src="${d}"
+          src="${m}"
           alt="${i}"
           >
         </div>
         <h3 class="book-title">${i}</h3>
-        <p class="book-author">${m}</p>
+        <p class="book-author">${g}</p>
       </li>
-    `}return e}function b(){const t=window.innerWidth;return t<768?"mobile":t<1200?"tablet":"desktop"}const v=[{title:"Save the Children",url:"https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis",img:"stc"},{title:"Project HOPE",url:"https://www.projecthope.org/country/ukraine/",img:"hope"},{title:"UNITED24",url:"https://u24.gov.ua/uk",img:"u24"},{title:"International Medical Corps",url:"https://internationalmedicalcorps.org/country/ukraine/",img:"imc"},{title:"Medicins Sans Frontieres",url:"https://www.msf.org/ukraine",img:"msf"},{title:"RAZOM",url:"https://www.razomforukraine.org/",img:"razom"},{title:"Action against hunger",url:"https://www.actionagainsthunger.org/location/europe/ukraine/",img:"aah"},{title:"World vision",url:"https://www.wvi.org/emergencies/ukraine",img:"wv"},{title:"Serhiy Prytula Charity Foundation",url:"https://prytulafoundation.org/en",img:"prytula"}];function E(t,e,o){t.classList.toggle(o),t.classList.contains(o)?e.scrollTop=e.scrollHeight:e.scrollTop=0}r.scrollBtn.addEventListener("click",()=>E(r.scrollBtn,r.supportList,"isDown"));g(M,r.supportList,v);function M(t){const{title:e,url:o,img:n}=t;return`
+    `}return e}function I(){const t=window.innerWidth;return t<768?"mobile":t<1200?"tablet":"desktop"}const S=[{title:"Save the Children",url:"https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis",img:"stc"},{title:"Project HOPE",url:"https://www.projecthope.org/country/ukraine/",img:"hope"},{title:"UNITED24",url:"https://u24.gov.ua/uk",img:"u24"},{title:"International Medical Corps",url:"https://internationalmedicalcorps.org/country/ukraine/",img:"imc"},{title:"Medicins Sans Frontieres",url:"https://www.msf.org/ukraine",img:"msf"},{title:"RAZOM",url:"https://www.razomforukraine.org/",img:"razom"},{title:"Action against hunger",url:"https://www.actionagainsthunger.org/location/europe/ukraine/",img:"aah"},{title:"World vision",url:"https://www.wvi.org/emergencies/ukraine",img:"wv"},{title:"Serhiy Prytula Charity Foundation",url:"https://prytulafoundation.org/en",img:"prytula"}];function $(t,e,o){t.classList.toggle(o),t.classList.contains(o)?e.scrollTop=e.scrollHeight:e.scrollTop=0}n.scrollBtn.addEventListener("click",()=>$(n.scrollBtn,n.supportList,"isDown"));d(B,n.supportList,S);function B(t){const{title:e,url:o,img:r}=t;return`
     <li class="support-item">
       <a
         class="support-link"
@@ -24,7 +24,7 @@ import{g as u,r,a as g}from"./assets/renderMarkup-e7327bcc.js";import"./assets/v
         arial-label="${e}"
         href="${o}"
       >
-        <img class="support-img" src="./img/support/${n}.png" alt="${e}" />
+        <img class="support-img" src="./img/support/${r}.png" alt="${e}" />
       </a>
-    </li>`}console.log("pop up");console.log("category-card");const a=document.querySelector(".checkbox-input"),c=document.body;a.addEventListener("change",function(){a.checked?c.classList.add("dark-theme"):c.classList.remove("dark-theme")});document.addEventListener("DOMContentLoaded",function(){const t=document.querySelector(".burger-menu-btn"),e=document.querySelector(".close-menu-btn"),o=document.querySelector(".header-nav");t.addEventListener("click",function(){t.classList.add("is-hidden"),e.classList.remove("is-hidden"),o.style.display="flex"}),e.addEventListener("click",function(){t.classList.remove("is-hidden"),e.classList.add("is-hidden"),o.style.display="none"})});
+    </li>`}console.log("pop up");console.log("category-card");const c=document.querySelector(".checkbox-input"),a=document.body;c.addEventListener("change",function(){c.checked?(a.classList.add("dark-theme"),localStorage.setItem("theme","dark-theme")):(a.classList.remove("dark-theme"),localStorage.setItem("theme","ldark-theme"))});window.onload=E;function E(){a.classList.add(localStorage.getItem("theme"));const t=document.querySelector(".header-nav").getElementsByTagName("a");for(let e=0;e<t.length;e++)document.location.href.indexOf(t[e].href)>=0&&(t[e].classList.add("active"),console.log(document.location.href))}
 //# sourceMappingURL=commonHelpers.js.map
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,9 +1,9 @@
-import{a as t,r,g as n}from"./assets/renderMarkup-e7327bcc.js";import"./assets/vendor-0cb09735.js";const e={isSupported(){return typeof Storage<"u"},has(o){return localStorage.hasOwnProperty(o)},get(o){let s=localStorage.getItem(o);if(typeof s!="string")return s;if(s!=="undefined")return s==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(s)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(s)?Number(s):s[0]==="{"||s[0]==="["?JSON.parse(s):s},set(o,s){if(typeof o!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${o}')`);(typeof s=="object"||Array.isArray(s))&&(s=JSON.stringify(s)),localStorage.setItem(o,s)},remove(o){localStorage.removeItem(o)}};e.remove("cart1");const a=async()=>{const o=await n("643282b1e85766588626a0b2"),s=[];s.push(o),e.set("cart",s)};a();if(e.has("cart")){const o=e.get("cart");console.log(o[0]),t(g,r.shoppingListMain,e.get("cart"))}else t(l);function l(o){return console.log(o),`<p class="shopping-list-container-text">
+import{a as t,r}from"./assets/renderMarkup-15bca558.js";const o={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let s=localStorage.getItem(e);if(typeof s!="string")return s;if(s!=="undefined")return s==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(s)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(s)?Number(s):s[0]==="{"||s[0]==="["?JSON.parse(s):s},set(e,s){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof s=="object"||Array.isArray(s))&&(s=JSON.stringify(s)),localStorage.setItem(e,s)},remove(e){localStorage.removeItem(e)}};o.remove("cart1");if(o.has("cart")){const e=o.get("cart");console.log(e[0]),t(a,r.shoppingListMain,o.get("cart"))}else t(n);function n(e){return console.log(e),`<p class="shopping-list-container-text">
             This page is empty, add some books <br />
             and proceed to order.
-          </p>`}function g({book_image:o,title:s,list_name:i}){return console.log(book),`<ul class="shopping-list">
+          </p>`}function a({book_image:e,title:s,list_name:i}){return console.log(book),`<ul class="shopping-list">
   <li class="shopping-item">
-    <img class="shopping-img" src="${o}" alt="${s}" width="100" height="142" />
+    <img class="shopping-img" src="${e}" alt="${s}" width="100" height="142" />
     <svg class="header-logo-icon-bookshelf" width="12" height="12">
       <use href="./img/icons/icons.svg#icon-bookshelf"></use>
     </svg>
