de2a1891c62c1d8cb1d5f9aa028049352989360c
VolodymyrKozel <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-25 08:38:11 +0000

    Deploying to gh-pages from @ VolodymyrKozel/renderrangers@9a27c3a0fe3c60e56cccdedda2032b53481ab42f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,21 +1,21 @@
-import{g as a,r as n,a as g}from"./assets/renderMarkup-41a964ef.js";import"./assets/vendor-0cb09735.js";const m=async()=>{const t=await a("category-list");t.unshift({list_name:"ALL CATEGORIES"}),console.log(t)};m();console.log("category-list");const l=document.getElementById("books-list"),h=async()=>{try{const t=await a("top-books"),e=d(t,4);w(e)}catch(t){console.error(t)}finally{}};h();function d(t,e){const o=[];for(;o.length<e;){const r=Math.floor(Math.random()*t.length);o.includes(r)||o.push(r)}return o.map(r=>t[r])}function w(t){const e=k(t);l.innerHTML="",l.appendChild(e)}function k(t){const e=document.createDocumentFragment();return t.forEach(({list_name:o,books:r})=>{const s=document.createElement("li");s.className="sellers-category",s.innerHTML=`
+import{g as u,r,a as g}from"./assets/renderMarkup-e7327bcc.js";import"./assets/vendor-0cb09735.js";const h=async()=>{const t=await u("category-list");t.unshift({list_name:"ALL CATEGORIES"}),console.log(t)};h();console.log("category-list");const l=document.getElementById("books-list"),k=async()=>{try{const t=await u("top-books"),e=f(t,4);w(e)}catch(t){console.error(t)}finally{}};k();function f(t,e){const o=[];for(;o.length<e;){const n=Math.floor(Math.random()*t.length);o.includes(n)||o.push(n)}return o.map(n=>t[n])}function w(t){const e=y(t);l.innerHTML="",l.appendChild(e)}function y(t){const e=document.createDocumentFragment();return t.forEach(({list_name:o,books:n})=>{const s=document.createElement("li");s.className="sellers-category",s.innerHTML=`
       <h2 class="sellers-category-title">${o}</h2>
       <ul class="sellers-category-list">
-        ${f(r)}
+        ${L(n)}
       </ul>
       <button class="sellers-button" type="button">see more</button>
-    `,e.appendChild(s)}),e}function f(t){let e="";const o=y(),r=o==="desktop"?5:o==="tablet"?3:1;for(let s=0;s<r&&s<t.length;s++){const{book_image:c,title:i,author:u,_id:p}=t[s];e+=`
+    `,e.appendChild(s)}),e}function L(t){let e="";const o=b(),n=o==="desktop"?5:o==="tablet"?3:1;for(let s=0;s<n&&s<t.length;s++){const{book_image:d,title:i,author:m,_id:p}=t[s];e+=`
       <li class="sellers-item" data-id="${p}">
         <div class="book-image-wrapper">
           <img class="book-image"
-          src="${c}"
+          src="${d}"
           alt="${i}"
           >
         </div>
         <h3 class="book-title">${i}</h3>
-        <p class="book-author">${u}</p>
+        <p class="book-author">${m}</p>
       </li>
-    `}return e}function y(){const t=window.innerWidth;return t<768?"mobile":t<1200?"tablet":"desktop"}const b=[{title:"Save the Children",url:"https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis",img:"stc"},{title:"Project HOPE",url:"https://www.projecthope.org/country/ukraine/",img:"hope"},{title:"UNITED24",url:"https://u24.gov.ua/uk",img:"u24"},{title:"International Medical Corps",url:"https://internationalmedicalcorps.org/country/ukraine/",img:"imc"},{title:"Medicins Sans Frontieres",url:"https://www.msf.org/ukraine",img:"msf"},{title:"RAZOM",url:"https://www.razomforukraine.org/",img:"razom"},{title:"Action against hunger",url:"https://www.actionagainsthunger.org/location/europe/ukraine/",img:"aah"},{title:"World vision",url:"https://www.wvi.org/emergencies/ukraine",img:"wv"},{title:"Serhiy Prytula Charity Foundation",url:"https://prytulafoundation.org/en",img:"prytula"}];function L(t,e,o){t.classList.toggle(o),t.classList.contains(o)?e.scrollTop=e.scrollHeight:e.scrollTop=0}n.scrollBtn.addEventListener("click",()=>L(n.scrollBtn,n.supportList,"isDown"));g($,n.supportList,b);function $(t){const{title:e,url:o,img:r}=t;return`
+    `}return e}function b(){const t=window.innerWidth;return t<768?"mobile":t<1200?"tablet":"desktop"}const v=[{title:"Save the Children",url:"https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis",img:"stc"},{title:"Project HOPE",url:"https://www.projecthope.org/country/ukraine/",img:"hope"},{title:"UNITED24",url:"https://u24.gov.ua/uk",img:"u24"},{title:"International Medical Corps",url:"https://internationalmedicalcorps.org/country/ukraine/",img:"imc"},{title:"Medicins Sans Frontieres",url:"https://www.msf.org/ukraine",img:"msf"},{title:"RAZOM",url:"https://www.razomforukraine.org/",img:"razom"},{title:"Action against hunger",url:"https://www.actionagainsthunger.org/location/europe/ukraine/",img:"aah"},{title:"World vision",url:"https://www.wvi.org/emergencies/ukraine",img:"wv"},{title:"Serhiy Prytula Charity Foundation",url:"https://prytulafoundation.org/en",img:"prytula"}];function E(t,e,o){t.classList.toggle(o),t.classList.contains(o)?e.scrollTop=e.scrollHeight:e.scrollTop=0}r.scrollBtn.addEventListener("click",()=>E(r.scrollBtn,r.supportList,"isDown"));g(M,r.supportList,v);function M(t){const{title:e,url:o,img:n}=t;return`
     <li class="support-item">
       <a
         class="support-link"
@@ -24,7 +24,7 @@ import{g as a,r as n,a as g}from"./assets/renderMarkup-41a964ef.js";import"./ass
         arial-label="${e}"
         href="${o}"
       >
-        <img class="support-img" src="./img/support/${r}.png" alt="${e}" />
+        <img class="support-img" src="./img/support/${n}.png" alt="${e}" />
       </a>
-    </li>`}console.log("pop up");console.log("category-card");
+    </li>`}console.log("pop up");console.log("category-card");const a=document.querySelector(".checkbox-input"),c=document.body;a.addEventListener("change",function(){a.checked?c.classList.add("dark-theme"):c.classList.remove("dark-theme")});document.addEventListener("DOMContentLoaded",function(){const t=document.querySelector(".burger-menu-btn"),e=document.querySelector(".close-menu-btn"),o=document.querySelector(".header-nav");t.addEventListener("click",function(){t.classList.add("is-hidden"),e.classList.remove("is-hidden"),o.style.display="flex"}),e.addEventListener("click",function(){t.classList.remove("is-hidden"),e.classList.add("is-hidden"),o.style.display="none"})});
 //# sourceMappingURL=commonHelpers.js.map
