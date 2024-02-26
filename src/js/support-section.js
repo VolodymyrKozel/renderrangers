e911918f0cbe4ea8fc7a10ed197ac243e6c84e3c
JeniaGurman <144277083+JeniaGurman@users.noreply.github.com>
2024-02-26 21:15:19 +0200

    list-3

--- src/js/support-section.js
+++ src/js/support-section.js
@@ -1,71 +1,71 @@
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
+// import Swiper from 'swiper';
+// import { Navigation } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
 
-import { supportList } from '../db/supportList';
-import { refs } from './refs';
+// import { supportList } from '../db/supportList';
+// import { refs } from './refs';
 
-renderSupportList(supportList);
+// renderSupportList(supportList);
 
-function renderSupportList(supportList) {
-  const listItems = supportList.map(createSupportItem);
-  refs.supportList.append(...listItems);
-}
+// function renderSupportList(supportList) {
+//   const listItems = supportList.map(createSupportItem);
+//   refs.supportList.append(...listItems);
+// }
 
-function createSupportItem(item, index) {
-  const { title, url, img, srcset } = item;
+// function createSupportItem(item, index) {
+//   const { title, url, img, srcset } = item;
 
-  const li = document.createElement('li');
-  li.classList.add('swiper-slide');
+//   const li = document.createElement('li');
+//   li.classList.add('swiper-slide');
 
-  const a = document.createElement('a');
-  a.classList.add('support-link');
-  a.target = '_blank';
-  a.rel = 'noopener noreferrer';
-  a.ariaLabel = title;
-  a.href = url;
+//   const a = document.createElement('a');
+//   a.classList.add('support-link');
+//   a.target = '_blank';
+//   a.rel = 'noopener noreferrer';
+//   a.ariaLabel = title;
+//   a.href = url;
 
-  const span = document.createElement('span');
-  span.classList.add('support-count');
-  span.textContent = `0${index + 1}`;
+//   const span = document.createElement('span');
+//   span.classList.add('support-count');
+//   span.textContent = `0${index + 1}`;
 
-  const imgEl = document.createElement('img');
-  imgEl.classList.add('support-img');
-  imgEl.src = `./img/support/${img}.png`;
-  imgEl.srcset = `./img/support/${srcset}`;
-  imgEl.alt = title;
+//   const imgEl = document.createElement('img');
+//   imgEl.classList.add('support-img');
+//   imgEl.src = `./img/support/${img}.png`;
+//   imgEl.srcset = `./img/support/${srcset}`;
+//   imgEl.alt = title;
 
-  a.append(span, imgEl);
+//   a.append(span, imgEl);
 
-  li.append(a);
-  return li;
-}
-const nextBtn = document.querySelector('.next-btn');
-const prevBtn = document.querySelector('.prev-btn');
+//   li.append(a);
+//   return li;
+// }
+// const nextBtn = document.querySelector('.next-btn');
+// const prevBtn = document.querySelector('.prev-btn');
 
-const swiper = new Swiper('.support-swiper', {
-  direction: 'vertical',
-  slidesPerView: 6,
-  slidesPerGroup: 6,
-  allowTouchMove: true,
-  spaceBetween: '20px',
+// const swiper = new Swiper('.support-swiper', {
+//   direction: 'vertical',
+//   slidesPerView: 6,
+//   slidesPerGroup: 6,
+//   allowTouchMove: true,
+//   spaceBetween: '20px',
 
-  modules: [Navigation],
-  navigation: {
-    nextEl: '.next-btn',
-    prevEl: '.prev-btn',
-    disabledClass: 'none',
-  },
-  on: {
-    slideChange: function () {
-      if (this.activeIndex === 0) {
-        prevBtn.classList.add('swiper-button-hidden');
-        nextBtn.classList.remove('swiper-button-hidden');
-      } else {
-        prevBtn.classList.remove('swiper-button-hidden');
-        nextBtn.classList.add('swiper-button-hidden');
-      }
-    },
-  },
-});
+//   modules: [Navigation],
+//   navigation: {
+//     nextEl: '.next-btn',
+//     prevEl: '.prev-btn',
+//     disabledClass: 'none',
+//   },
+//   on: {
+//     slideChange: function () {
+//       if (this.activeIndex === 0) {
+//         prevBtn.classList.add('swiper-button-hidden');
+//         nextBtn.classList.remove('swiper-button-hidden');
+//       } else {
+//         prevBtn.classList.remove('swiper-button-hidden');
+//         nextBtn.classList.add('swiper-button-hidden');
+//       }
+//     },
+//   },
+// });
