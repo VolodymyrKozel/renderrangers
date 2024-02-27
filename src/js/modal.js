fec4fff9ea24de59dabe3981736fcefed6c171d0
Volodymyr <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-27 11:14:39 +0100

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,6 @@
 import { getDataBooks } from './Api/uBooksApi';
-import renderMarkup from './helpers/renderMarkup';
 import LS from './helpers/localStorageHelper';
+import modalTemplate from './template/modalTemplate';
 
 const body = document.querySelector('body');
 const modalBackdrop = document.querySelector('.backdrop');
@@ -107,50 +107,4 @@ function escapeCloseModal(event) {
 function closeModal() {
   modalBackdrop.style.display = 'none';
   body.style.overflow = 'auto';
-}
-
-//template
-
-function modalTemplate({
-  _id,
-  book_image,
-  title,
-  author,
-  description,
-  buy_links,
-}) {
-  return `
-  <section class="modalmenu">
-      <button type="button" id="modal-close-id" class="close-button modal-close-button">
-      <svg class="close-icon" width="24" height="24">
-        <use src="./img/icons/icons.svg#icon-x-close"></use>
-      </svg>
-    </button>
-  <div class="modal-container">
-  <img src="${book_image}" class="modal-image">
-    <div class="modal-wrap">
-      <h2 class="modal-title">${title}</h2>
-      <p class="modal-author">${author}</p>
-      <p class="description">${description}</p>
-        <ul class="buy-links-list">
-      ${renderMarkup(buyLinksTemplate, buy_links.slice(0, 2))}
-        </ul>       
-    </div>
-  </div>
-   <button type="button" id="modal-list-button-id" data-id="${_id}">
-      Add this book to shopping list
-    </button>
-    </section>
-`;
-}
-function buyLinksTemplate({ name, url }) {
-  return `
-    <li class="buy-links-item">
-    <a target="_blank" rel="noopener noreferrer" aria-label="${name}" href=${url}>
-      <img src="./img/${name.split(' ')[0]}-1x.jpg"  srcset="./img/${
-    name.split(' ')[0]
-  }-2x.png 2x" alt="${name}" class="platform-image">
-</a>
-</li>
-`;
 }
---
+++ src/js/template/modalTemplate.js
@@ -0,0 +1,45 @@
+import renderMarkup from '../helpers/renderMarkup';
+export default function modalTemplate({
+  _id,
+  book_image,
+  title,
+  author,
+  description,
+  buy_links,
+}) {
+  return `
+  <section class="modalmenu">
+      <button type="button" id="modal-close-id" class="close-button modal-close-button">
+      <svg class="close-icon" width="24" height="24">
+        <use src="./img/icons/icons.svg#icon-x-close"></use>
+      </svg>
+    </button>
+  <div class="modal-container">
+  <img src="${book_image}" class="modal-image">
+    <div class="modal-wrap">
+      <h2 class="modal-title">${title}</h2>
+      <p class="modal-author">${author}</p>
+      <p class="description">${description}</p>
+        <ul class="buy-links-list">
+      ${renderMarkup(buyLinksTemplate, buy_links.slice(0, 2))}
+        </ul>       
+    </div>
+  </div>
+   <button type="button" id="modal-list-button-id" data-id="${_id}">
+      Add this book to shopping list
+    </button>
+    </section>
+`;
+}
+
+function buyLinksTemplate({ name, url }) {
+  return `
+    <li class="buy-links-item">
+    <a target="_blank" rel="noopener noreferrer" aria-label="${name}" href=${url}>
+      <img src="./img/${name.split(' ')[0]}-1x.jpg"  srcset="./img/${
+    name.split(' ')[0]
+  }-2x.png 2x" alt="${name}" class="platform-image">
+</a>
+</li>
+`;
+}
