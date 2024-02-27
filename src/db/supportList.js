204ced8e235d6370b59e849f2c9a72f5398e1237
Volodymyr <Volodumurvel@gmail.com>
2024-02-27 21:24:11 +0100

    db

---
+++ src/db/supportList.js
@@ -0,0 +1,75 @@
+import stc from '../img/support/stc.png';
+import stc2x from '../img/support/stc@2x.png';
+import hope from '../img/support/hope.png';
+import hope2x from '../img/support/hope@2x.png';
+import u24 from '../img/support/u24.png';
+import u242x from '../img/support/u24@2x.png';
+import imc from '../img/support/imc.png';
+import imc2x from '../img/support/imc@2x.png';
+import msf from '../img/support/msf.png';
+import msf2x from '../img/support/msf@2x.png';
+import razom from '../img/support/razom.png';
+import razom2x from '../img/support/razom@2x.png';
+import aah from '../img/support/aah.png';
+import aah2x from '../img/support/aah@2x.png';
+import wv from '../img/support/wv.png';
+import wv2x from '../img/support/wv@2x.png';
+import prytula from '../img/support/prytula.png';
+import prytula2x from '../img/support/prytula@2x.png';
+
+export const supportList = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: stc,
+    srcset: stc2x,
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: 'hope',
+    srcset: 'hope@2x.png 2x',
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: 'u24',
+    srcset: 'u24@2x.png 2x',
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: 'imc',
+    srcset: 'imc@2x.png 2x',
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: 'msf',
+    srcset: 'msf@2x.png 2x',
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: 'razom',
+    srcset: 'razom@2x.png 2x',
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: 'aah',
+    srcset: 'aah@2x.png 2x',
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: 'wv',
+    srcset: 'wv@2x.png 2x',
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: 'prytula',
+    srcset: 'prytula@2x.png 2x',
+  },
+];
