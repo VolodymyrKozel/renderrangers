d8b48dbea144784e2c448f49901cfe41030daeda
AnnaP-G <klientkv1511@gmail.com>
2024-02-27 18:33:43 +0200

    header7

--- src/js/header.js
+++ src/js/header.js
@@ -2,61 +2,51 @@ const checkboxInput = document.querySelector('.checkbox-input');
 const body = document.body;
 
 checkboxInput.addEventListener('change', function () {
-  if (checkboxInput.checked) {
-    body.classList.add('dark-theme');
-    localStorage.setItem('theme', 'dark-theme');
-  } else {
-    body.classList.remove('dark-theme');
-    localStorage.setItem('theme', '');
-  }
+    if (checkboxInput.checked) {
+        body.classList.add('dark-theme');
+        localStorage.setItem('theme', 'dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+        localStorage.setItem('theme', '');
+    }
 });
 
-  const checkbox = document.getElementById('checkbox');
-  const owlIcon = document.querySelector('.owl-icon');
-  const owlLightIcon = document.querySelector('.owl-light-icon');
+const checkbox = document.getElementById('checkbox');
+const owlIcon = document.querySelector('.owl-icon');
+const owlLightIcon = document.querySelector('.owl-light-icon');
 
-  checkbox.addEventListener('change', function () {
+checkbox.addEventListener('change', function () {
     if (checkbox.checked) {
-      owlIcon.style.display = 'block';
-      owlLightIcon.style.display = 'none';
+        owlIcon.style.display = 'block';
+        owlLightIcon.style.display = 'none';
     } else {
-      owlIcon.style.display = 'none';
-      owlLightIcon.style.display = 'block';
+        owlIcon.style.display = 'none';
+        owlLightIcon.style.display = 'block';
     }
-  });
+});
 
-  
 window.onload = setActive;
+
 function setActive() {
-  let theme = localStorage.getItem('theme');
-  if (theme === 'dark-theme') {
-    document.body.classList.add(theme);
-    checkboxInput.checked = true;
-  }
-  const aObj = document.querySelector('.header-nav').getElementsByTagName('a');
-  for (let i = 0; i < aObj.length; i++) {
-    if (document.location.href.indexOf(aObj[i].href) >= 0) {
-      aObj[i].classList.add('active');
-      console.log(document.location.href);
+    let theme = localStorage.getItem('theme');
+    if (theme === 'dark-theme') {
+        body.classList.add('dark-theme');
+        checkboxInput.checked = true;
     }
-  }
+
+    const aObj = document.querySelector('.header-nav').getElementsByTagName('a');
+    for (let i = 0; i < aObj.length; i++) {
+        if (document.location.href.indexOf(aObj[i].href) >= 0) {
+            aObj[i].classList.add('active');
+            console.log(document.location.href);
+        }
+    }
 }
 
-// const burgerMenuBtn = document.querySelector('.burger-menu-btn');
-// const mobilMenu = document.querySelector('.mobile-menu-container');
-// burgerMenuBtn.addEventListener('click', function () {
-//   mobilMenu.classList.add('open-menu');
-// });
+  const burgerMenuBtn = document.querySelector('.header-burger');
+  const mobileMenuContainer = document.querySelector('.mobile-menu-container');
 
+  burgerMenuBtn.addEventListener('click', () => {
+    mobileMenuContainer, burgerMenuBtn.classList.toggle('open-menu');
+  });
 
-const burgerMenuBtn = document.querySelector('.burger-menu-btn');
-const mobileMenuContainer = document.querySelector('.mobile-menu-container');
-const closeMenuButton = document.querySelector('.mob-menu-close-btn');
-
-burgerMenuBtn.addEventListener('click', () => {
-  mobileMenuContainer.classList.add('open-menu');
-});
-
-closeMenuButton.addEventListener('click', () => {
-  mobileMenuContainer.classList.remove('open-menu');
-});
