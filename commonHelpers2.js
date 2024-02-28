3a22c3c5cbf2d10077275a9cc7cdd9665d141455
VolodymyrKozel <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-28 19:14:58 +0000

    Deploying to gh-pages from @ VolodymyrKozel/renderrangers@dee606a5f47464bbe5149a8e1c1c2b7bd9fd7a1b 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{c as d,d as f,e as r,r as n,b as h}from"./assets/header-dd4c875e.js";import{P as u}from"./assets/vendor-94a85c0e.js";const k=document.querySelector("#tui-pagination-container");let l=2,c=4;window.innerWidth>=768&&(l=3,c=3);const y={itemsPerPage:c,visiblePages:l},$=new u(k,y);$.movePageTo(1);const p={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}};function b({_id:e,list_name:t,author:o,title:s,book_image:i,description:g,buy_links:m}){return`<li class="shopping-item" data-id="${e}">
+import{c as m,d,e as f,f as h,r as n,b as u}from"./assets/header-e35e4aad.js";import{P as k}from"./assets/vendor-94a85c0e.js";const y=document.querySelector("#tui-pagination-container");let r=2,l=4;window.innerWidth>=768&&(r=3,l=3);const $={itemsPerPage:l,visiblePages:r},b=new k(y,$);b.movePageTo(1);const c={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}};function S({_id:e,list_name:t,author:o,title:s,book_image:i,description:p,buy_links:g}){return`<li class="shopping-item" data-id="${e}">
             <img
               class="shopping-img"
               src="${i}"
@@ -21,22 +21,22 @@ import{c as d,d as f,e as r,r as n,b as h}from"./assets/header-dd4c875e.js";impo
                   </svg>
                 </button>
               </div>
-              <p class="descr-shopping">${g}</p>
+              <p class="descr-shopping">${p}</p>
               <div class="author-info-container">
                 <p class="author-info">${o}</p>
-                ${S(m)}
+                ${v(g)}
               </div>
             </div>
-          </li>`}function S(e){const t=e.find(s=>s.name==="Amazon").url,o=e.find(s=>s.name==="Apple Books").url;return`<ul class="shop-link-list">
+          </li>`}function v(e){const t=e.find(s=>s.name==="Amazon").url,o=e.find(s=>s.name==="Apple Books").url;return`<ul class="shop-link-list">
               <li class=""shop-link-item>
                 <a class="shop-link-amazon" href="${t}" target="_blank">
-                   <img src="${d}" srcset="${f} 2x" alt="Amazon" class="amazon-image">
+                   <img src="${m}" srcset="${d} 2x" alt="Amazon" class="amazon-image">
                 </a>
               </li>
               <li class=""shop-link-item>
                 <a class="shop-link-amazon" href="${o}" target="_blank">
-                    <img src="${r}" srcset="${r} 2x" alt="Apple-books" class="apple-books-image">
+                    <img src="${f}" srcset="${h} 2x" alt="Apple-books" class="apple-books-image">
                 </a>
               </li>
-          </ul>`}const a="cart";function v(){n.shoppingListMain.innerHTML="";const e=p.get(a);if(!e||!e.length)n.shoppingEmpty.classList.remove("hidden");else{n.shoppingText.style.marginBottom="40px",n.shoppingEmpty.classList.add("hidden");const o=h(b,e);n.shoppingListMain.innerHTML=o,document.querySelector(".js-shopping-main").addEventListener("click",w)}}function w(e){if(e.target.tagName==="BUTTON"||e.target.tagName==="svg"||e.target.tagName==="use"){const t=e.target.closest(".shopping-item"),s=p.get(a).filter(({_id:i})=>i!==t.dataset.id);localStorage.setItem(a,JSON.stringify(s)),t.remove()}}v();window.innerWidth<1440&&(console.log("1440"),document.querySelector(".aside").style.display="none");
+          </ul>`}const a="cart";function w(){n.shoppingListMain.innerHTML="";const e=c.get(a);if(!e||!e.length)n.shoppingEmpty.classList.remove("hidden");else{n.shoppingText.style.marginBottom="40px",n.shoppingEmpty.classList.add("hidden");const o=u(S,e);n.shoppingListMain.innerHTML=o,document.querySelector(".js-shopping-main").addEventListener("click",L)}}function L(e){if(e.target.tagName==="BUTTON"||e.target.tagName==="svg"||e.target.tagName==="use"){const t=e.target.closest(".shopping-item"),s=c.get(a).filter(({_id:i})=>i!==t.dataset.id);localStorage.setItem(a,JSON.stringify(s)),t.remove()}}w();window.innerWidth<1440&&(console.log("1440"),document.querySelector(".aside").style.display="none");
 //# sourceMappingURL=commonHelpers2.js.map
