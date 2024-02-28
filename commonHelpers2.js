9d475435c1066eca753bcb1f6ac6f4e2602dc0b2
VolodymyrKozel <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-28 14:39:24 +0000

    Deploying to gh-pages from @ VolodymyrKozel/renderrangers@d48f40686370aa6c5d008b9273810aa475fa9ba9 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,14 +1,14 @@
-import{r as s,b as g}from"./assets/header-5cf470a5.js";import{P as c}from"./assets/vendor-94a85c0e.js";const m=document.querySelector("#tui-pagination-container");let o=2,a=4;window.innerWidth>=768&&(o=3,a=3);const d={itemsPerPage:a,visiblePages:o},h=new c(m,d);h.movePageTo(1);const u={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}};function f({_id:e,list_name:t,author:n,title:i,book_image:r,description:p,buy_links:l}){return`<li class="shopping-item" data-id="${e}">
+import{r as s,b as m}from"./assets/header-5cf470a5.js";import{P as d}from"./assets/vendor-94a85c0e.js";const f=document.querySelector("#tui-pagination-container");let r=2,l=4;window.innerWidth>=768&&(r=3,l=3);const u={itemsPerPage:l,visiblePages:r},h=new d(f,u);h.movePageTo(1);const p={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}};function k({_id:e,list_name:t,author:o,title:n,book_image:i,description:c,buy_links:g}){return`<li class="shopping-item" data-id="${e}">
             <img
               class="shopping-img"
-              src="${r}"
-              alt="${i}"
+              src="${i}"
+              alt="${n}"
             />
             <div class="shopping-wrap">
               <div class="heading-info">
                 <div class="title-container">
                   <h2 class="title-book">
-                    ${i}
+                    ${n}
                   </h2>
                   <p class="category-book">${t}</p>
                 </div>
@@ -21,22 +21,22 @@ import{r as s,b as g}from"./assets/header-5cf470a5.js";import{P as c}from"./asse
                   </svg>
                 </button>
               </div>
-              <p class="descr-shopping">${p}</p>
+              <p class="descr-shopping">${c}</p>
               <div class="author-info-container">
-                <p class="author-info">${n}</p>
-                ${b(l)}
+                <p class="author-info">${o}</p>
+                ${b(g)}
               </div>
             </div>
-          </li>`}function b(e){const t=e.find(i=>i.name==="Amazon").url,n=e.find(i=>i.name==="Apple Books").url;return`<ul class="shop-link-list">
+          </li>`}function b(e){const t=e.find(n=>n.name==="Amazon").url,o=e.find(n=>n.name==="Apple Books").url;return`<ul class="shop-link-list">
               <li class=""shop-link-item>
                 <a class="shop-link-amazon" href="${t}" target="_blank">
                    <img src="../img/amazon-1x.png" srcset="../img/amazon-2x.png 2x" alt="Amazon" class="amazon-image">
                 </a>
               </li>
               <li class=""shop-link-item>
-                <a class="shop-link-amazon" href="${n}" target="_blank">
+                <a class="shop-link-amazon" href="${o}" target="_blank">
                     <img src="../img/apple-1x.png" srcset="../img/apple-2x.png 2x" alt="Apple-books" class="apple-books-image">
                 </a>
               </li>
-          </ul>`}function k(){s.shoppingListMain.innerHTML="";let e=u.get("cart");if(!e||!e.length)s.shoppingEmpty.classList.remove("hidden");else{s.shoppingText.style.marginBottom="40px",s.shoppingEmpty.classList.add("hidden");const n=g(f,e);s.shoppingListMain.innerHTML=n}}k();
+          </ul>`}const a="cart";function y(){s.shoppingListMain.innerHTML="";const e=p.get(a);if(!e||!e.length)return;s.shoppingText.style.marginBottom="40px",s.shoppingEmpty.classList.add("hidden");const o=m(k,e);s.shoppingListMain.innerHTML=o,document.querySelector(".js-shopping-main").addEventListener("click",S)}function S(e){if(e.target.tagName==="BUTTON"||e.target.tagName==="svg"||e.target.tagName==="use"){const t=e.target.closest(".shopping-item"),n=p.get(a).filter(({_id:i})=>i!==t.dataset.id);localStorage.setItem(a,JSON.stringify(n)),t.remove()}}y();window.innerWidth<1440&&(console.log("1440"),document.querySelector(".aside").style.display="none");
 //# sourceMappingURL=commonHelpers2.js.map
