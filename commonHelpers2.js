24b62667c8921015fe74b6193c1a6988316d333e
VolodymyrKozel <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-28 09:13:39 +0000

    Deploying to gh-pages from @ VolodymyrKozel/renderrangers@620681c41d964e11696e21c3bdd33b55a8f9051c 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,7 +1,7 @@
-import{a as o,r as l}from"./assets/header-78493582.js";const p={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}},c="cart";function g({_id:e,list_name:t,author:n,title:s,book_image:i,description:a,buy_links:r}){return`<li class="shopping-item" data-id="${e}">
+import{a as n,r as c}from"./assets/header-78493582.js";const r={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}},i="cart";function g({_id:e,list_name:t,author:o,title:s,book_image:a,description:l,buy_links:p}){return`<li class="shopping-item" data-id="${e}">
             <img
               class="shopping-img"
-              src="${i}"
+              src="${a}"
               alt="${s}"
             />
             <div class="shopping-wrap">
@@ -21,22 +21,22 @@ import{a as o,r as l}from"./assets/header-78493582.js";const p={isSupported(){re
                   </svg>
                 </button>
               </div>
-              <p class="descr-shopping">${a}</p>
+              <p class="descr-shopping">${l}</p>
               <div class="author-info-container">
-                <p class="author-info">${n}</p>
-                ${m(r)}
+                <p class="author-info">${o}</p>
+                ${m(p)}
               </div>
             </div>
-          </li>`}function m(e){const t=e.find(s=>s.name==="Amazon").url,n=e.find(s=>s.name==="Apple Books").url;return`<ul class="shop-link-list">
+          </li>`}function m(e){const t=e.find(s=>s.name==="Amazon").url,o=e.find(s=>s.name==="Apple Books").url;return`<ul class="shop-link-list">
               <li class=""shop-link-item>
                 <a class="shop-link-amazon" href="${t}" target="_blank">
                    <img src="./img/amazon.png" alt="Amazon" class="amazon-image">
                 </a>
               </li>
               <li class=""shop-link-item>
-                <a class="shop-link-amazon" href="${n}" target="_blank">
+                <a class="shop-link-amazon" href="${o}" target="_blank">
                     <img src="./img/apple-1x.png" alt="Apple-books" class="apple-books-image">
                 </a>
               </li>
-          </ul>`}function d(){o.shoppingListMain.innerHTML="";let e=p.get(c);if(!e||!e.length)return;o.shoppingText.style.marginBottom="40px",o.shoppingEmpty.classList.add("hidden");const n=l(g,e);o.shoppingListMain.innerHTML=n;const s=document.querySelector(".shop-link-list");console.log(s),s.addEventListener("click",u)}function u(e){console.log(e.currentTarget)}d();
+          </ul>`}function d(){n.shoppingListMain.innerHTML="";let e=r.get(i);if(!e||!e.length)return;n.shoppingText.style.marginBottom="40px",n.shoppingEmpty.classList.add("hidden");const o=c(g,e);n.shoppingListMain.innerHTML=o;const s=document.querySelector(".js-shopping-main");console.log(s),s.addEventListener("click",f)}function f(e){if(e.target.tagName==="BUTTON"||e.target.tagName==="svg"||e.target.tagName==="use"){const t=e.target.closest(".shopping-item"),s=r.get(i).filter(({_id:a})=>a!==t.dataset.id);localStorage.setItem(i,JSON.stringify(s)),t.remove()}}d();
 //# sourceMappingURL=commonHelpers2.js.map
