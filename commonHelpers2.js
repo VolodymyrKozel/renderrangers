af7c5b836655a0e79f50ca5f78abc17c7e79b0b2
VolodymyrKozel <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-28 11:11:13 +0000

    Deploying to gh-pages from @ VolodymyrKozel/renderrangers@4ad4aa6d23938e48ea3bf34fabd3e36a16c508c6 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,14 +1,14 @@
-import{a as i,r as c}from"./assets/header-b73bccc1.js";import{P as g}from"./assets/vendor-f6623f5a.js";const m=document.querySelector("#pagination"),u={itemsPerPage:4,visiblePages:2,page:1,centerAlign:!0,firstItemClassName:"tui-first-child",lastItemClassName:"tui-last-child",template:{page:'<a href="#" class="tui-page-button">{{page}}</a>',currentPage:'<strong class="tui-page-button tui-is-selected">{{page}}</strong>',moreButton:'<a href="#" class="tui-page-button tui-{{type}}-is-ellip"><span class="tui-ico-ellip">...</span></a>'}};new g(m,u);const r={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}},o="cart";function d({_id:e,list_name:t,author:a,title:s,book_image:n,description:l,buy_links:p}){return`<li class="shopping-item" data-id="${e}">
+import{r as i,b as m}from"./assets/header-b7778bbb.js";import{P as d}from"./assets/vendor-94a85c0e.js";const u=document.querySelector("#tui-pagination-container");let r=2,l=4;window.innerWidth>=768&&(r=3,l=3);const f={itemsPerPage:l,visiblePages:r},h=new d(u,f);h.movePageTo(1);const c={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}},a="cart";function k({_id:e,list_name:t,author:o,title:n,book_image:s,description:p,buy_links:g}){return`<li class="shopping-item" data-id="${e}">
             <img
               class="shopping-img"
-              src="${n}"
-              alt="${s}"
+              src="${s}"
+              alt="${n}"
             />
             <div class="shopping-wrap">
               <div class="heading-info">
                 <div class="title-container">
                   <h2 class="title-book">
-                    ${s}
+                    ${n}
                   </h2>
                   <p class="category-book">${t}</p>
                 </div>
@@ -21,22 +21,22 @@ import{a as i,r as c}from"./assets/header-b73bccc1.js";import{P as g}from"./asse
                   </svg>
                 </button>
               </div>
-              <p class="descr-shopping">${l}</p>
+              <p class="descr-shopping">${p}</p>
               <div class="author-info-container">
-                <p class="author-info">${a}</p>
-                ${f(p)}
+                <p class="author-info">${o}</p>
+                ${b(g)}
               </div>
             </div>
-          </li>`}function f(e){const t=e.find(s=>s.name==="Amazon").url,a=e.find(s=>s.name==="Apple Books").url;return`<ul class="shop-link-list">
+          </li>`}function b(e){const t=e.find(n=>n.name==="Amazon").url,o=e.find(n=>n.name==="Apple Books").url;return`<ul class="shop-link-list">
               <li class=""shop-link-item>
                 <a class="shop-link-amazon" href="${t}" target="_blank">
                    <img src="./img/amazon.png" alt="Amazon" class="amazon-image">
                 </a>
               </li>
               <li class=""shop-link-item>
-                <a class="shop-link-amazon" href="${a}" target="_blank">
+                <a class="shop-link-amazon" href="${o}" target="_blank">
                     <img src="./img/apple-1x.png" alt="Apple-books" class="apple-books-image">
                 </a>
               </li>
-          </ul>`}function h(){i.shoppingListMain.innerHTML="";let e=r.get(o);if(!e||!e.length)return;i.shoppingText.style.marginBottom="40px",i.shoppingEmpty.classList.add("hidden");const a=c(d,e);i.shoppingListMain.innerHTML=a;const s=document.querySelector(".js-shopping-main");console.log(s),s.addEventListener("click",k)}function k(e){if(e.target.tagName==="BUTTON"||e.target.tagName==="svg"||e.target.tagName==="use"){const t=e.target.closest(".shopping-item"),s=r.get(o).filter(({_id:n})=>n!==t.dataset.id);localStorage.setItem(o,JSON.stringify(s)),t.remove()}}h();window.innerWidth<1440&&(console.log("1440"),document.querySelector(".aside").style.display="none");
+          </ul>`}function y(){i.shoppingListMain.innerHTML="";let e=c.get(a);if(!e||!e.length)return;i.shoppingText.style.marginBottom="40px",i.shoppingEmpty.classList.add("hidden");const o=m(k,e);i.shoppingListMain.innerHTML=o;const n=document.querySelector(".js-shopping-main");console.log(n),n.addEventListener("click",S)}function S(e){if(e.target.tagName==="BUTTON"||e.target.tagName==="svg"||e.target.tagName==="use"){const t=e.target.closest(".shopping-item"),n=c.get(a).filter(({_id:s})=>s!==t.dataset.id);localStorage.setItem(a,JSON.stringify(n)),t.remove()}}y();window.innerWidth<1440&&(console.log("1440"),document.querySelector(".aside").style.display="none");
 //# sourceMappingURL=commonHelpers2.js.map
