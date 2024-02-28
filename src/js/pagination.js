0cd061f60e2ece49f22fdf6e20ea2bcf5dd92230
Merylly <142231027+Merylly@users.noreply.github.com>
2024-02-28 12:56:50 +0200

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,7 +2,6 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 import '../css/pagination.css';
 
-import { getBookById } from './modal';
 
 const pagContainer = document.querySelector('#tui-pagination-container');
 
@@ -13,15 +12,15 @@ if (window.innerWidth >= 768) {
   itemsPerPage = 3;
 }
 
-// const shoppingList = localStorage.getItem('shoppingList');
-// console.log(shoppingList);
-// const listObj = JSON.parse(shoppingList);
+const shoppingList = localStorage.getItem('shoppingList');
+console.log(shoppingList);
+const listObj = JSON.parse(shoppingList);
 
-// const shoppingListArr = Object.keys(listObj);
-// console.log(shoppingListArr);
+const shoppingListArr = Object.keys(listObj);
+console.log(shoppingListArr);
 
 const options = {
-  // totalItems: shoppingListArr.length,
+  totalItems: shoppingListArr.length,
   itemsPerPage,
   visiblePages,
 };
@@ -35,7 +34,7 @@ export const pagination = new Pagination(pagContainer, options);
 
 //   const currentPage = event.page;
 
-//   getBookById(
+//   renderItem(
 //     shoppingListArr.slice(
 //       (currentPage - 1) * itemsPerPage,
 //       currentPage * itemsPerPage
