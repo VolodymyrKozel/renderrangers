dc46e791707bf52141b74a84b1f493a3ac52f0b3
VolodymyrKozel <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-28 14:04:50 +0000

    Deploying to gh-pages from @ VolodymyrKozel/renderrangers@3fc7419c435e8fca6300936f8776beb3e3f74419 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,14 +1,14 @@
-import{r as i,b as m}from"./assets/header-c0a89397.js";import{P as d}from"./assets/vendor-94a85c0e.js";const u=document.querySelector("#tui-pagination-container");let r=2,l=4;window.innerWidth>=768&&(r=3,l=3);const f={itemsPerPage:l,visiblePages:r},h=new d(u,f);h.movePageTo(1);const c={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}},a="cart";function k({_id:e,list_name:t,author:o,title:n,book_image:s,description:p,buy_links:g}){return`<li class="shopping-item" data-id="${e}">
+import{r as s,b as g}from"./assets/header-72619365.js";import{P as c}from"./assets/vendor-94a85c0e.js";const m=document.querySelector("#tui-pagination-container");let o=2,a=4;window.innerWidth>=768&&(o=3,a=3);const d={itemsPerPage:a,visiblePages:o},h=new c(m,d);h.movePageTo(1);const u={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}};function f({_id:e,list_name:t,author:n,title:i,book_image:r,description:p,buy_links:l}){return`<li class="shopping-item" data-id="${e}">
             <img
               class="shopping-img"
-              src="${s}"
-              alt="${n}"
+              src="${r}"
+              alt="${i}"
             />
             <div class="shopping-wrap">
               <div class="heading-info">
                 <div class="title-container">
                   <h2 class="title-book">
-                    ${n}
+                    ${i}
                   </h2>
                   <p class="category-book">${t}</p>
                 </div>
@@ -17,26 +17,26 @@ import{r as i,b as m}from"./assets/header-c0a89397.js";import{P as d}from"./asse
                   
                 >
                   <svg class="trash-btn-icon" height="16" width="16">
-                    <use href="./img/icons/icons.svg#icon-trash"></use>
+                    <use href="../img/icons/icons.svg#icon-trash"></use>
                   </svg>
                 </button>
               </div>
               <p class="descr-shopping">${p}</p>
               <div class="author-info-container">
-                <p class="author-info">${o}</p>
-                ${b(g)}
+                <p class="author-info">${n}</p>
+                ${b(l)}
               </div>
             </div>
-          </li>`}function b(e){const t=e.find(n=>n.name==="Amazon").url,o=e.find(n=>n.name==="Apple Books").url;return`<ul class="shop-link-list">
+          </li>`}function b(e){const t=e.find(i=>i.name==="Amazon").url,n=e.find(i=>i.name==="Apple Books").url;return`<ul class="shop-link-list">
               <li class=""shop-link-item>
                 <a class="shop-link-amazon" href="${t}" target="_blank">
-                   <img src="./img/amazon.png" alt="Amazon" class="amazon-image">
+                   <img src="../img/amazon-1x.png" srcset="../img/amazon-2x.png 2x" alt="Amazon" class="amazon-image">
                 </a>
               </li>
               <li class=""shop-link-item>
-                <a class="shop-link-amazon" href="${o}" target="_blank">
-                    <img src="./img/apple-1x.png" alt="Apple-books" class="apple-books-image">
+                <a class="shop-link-amazon" href="${n}" target="_blank">
+                    <img src="../img/apple-1x.png" srcset="../img/apple-2x.png 2x" alt="Apple-books" class="apple-books-image">
                 </a>
               </li>
-          </ul>`}function y(){i.shoppingListMain.innerHTML="";let e=c.get(a);if(!e||!e.length)return;i.shoppingText.style.marginBottom="40px",i.shoppingEmpty.classList.add("hidden");const o=m(k,e);i.shoppingListMain.innerHTML=o;const n=document.querySelector(".js-shopping-main");console.log(n),n.addEventListener("click",S)}function S(e){if(e.target.tagName==="BUTTON"||e.target.tagName==="svg"||e.target.tagName==="use"){const t=e.target.closest(".shopping-item"),n=c.get(a).filter(({_id:s})=>s!==t.dataset.id);localStorage.setItem(a,JSON.stringify(n)),t.remove()}}y();window.innerWidth<1440&&(console.log("1440"),document.querySelector(".aside").style.display="none");
+          </ul>`}function k(){s.shoppingListMain.innerHTML="";let e=u.get("cart");if(!e||!e.length)s.shoppingEmpty.classList.remove("hidden");else{s.shoppingText.style.marginBottom="40px",s.shoppingEmpty.classList.add("hidden");const n=g(f,e);s.shoppingListMain.innerHTML=n}}k();
 //# sourceMappingURL=commonHelpers2.js.map
