4379fe959cf02386f301f23a2155c99a7b43cf34
Merylly <maria.baltichouse.rv@gmail.com>
2024-02-28 21:13:04 +0200

    fix pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,10 +2,11 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 import '../css/pagination.css';
 
-import { renderItem } from './shopping-list'
-
 export function createPagination(totalItems) {
+  // контейнер для пагінації
   const pagContainer = document.querySelector('#tui-pagination-container');
+
+  // значення для різної ширини екрану
   let visiblePages = 2;
   let itemsPerPage = 4;
   if (window.innerWidth >= 768) {
@@ -13,14 +14,18 @@ export function createPagination(totalItems) {
     itemsPerPage = 3;
   }
 
+  // налаштування для ширини екрану
   const options = {
     totalItems,
     itemsPerPage,
     visiblePages,
     centerAlign: true,
   };
+
+  // створення пагінації
   const pagination = new Pagination(pagContainer, options);
 
+  // слухач переходу по сторінках
   pagination.on('afterMove', event => {
     const currentPage = event.page;
     onPageChange(currentPage);
@@ -29,30 +34,10 @@ export function createPagination(totalItems) {
   return pagination;
 }
 
+// функція для рендеру і переходу по сторінках
 function onPageChange(totalItems) {
- renderItem(totalItems);
+  //  renderItem(totalItems);
 }
 
-// const shoppingList = localStorage.getItem('shoppingList');
-// console.log(shoppingList);
-// const listObj = JSON.parse(shoppingList);
-
-// const shoppingListArr = Object.keys(listObj);
-// console.log(shoppingListArr);
-
-// pagination.on('beforeMove', event => {
-//   if (shoppingListArr.length / itemsPerPage <= 1) {
-//     return;
-//   }
-
-//   const currentPage = event.page;
-
-//   getBookById(
-//     shoppingListArr.slice(
-//       (currentPage - 1) * itemsPerPage,
-//       currentPage * itemsPerPage
-//     )
-//   );
-// });
-
-// pagination.movePageTo(1);
+// тест
+createPagination(20);
