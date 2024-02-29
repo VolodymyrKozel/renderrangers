33d89589e7ec914dc8a7a15c70d5bcbb9186369e
VolodymyrKozel <39493622+VolodymyrKozel@users.noreply.github.com>
2024-02-29 15:40:19 +0000

    Deploying to gh-pages from @ VolodymyrKozel/renderrangers@f64d9644318ae5c8d0da7dc938a6f36a7d5e48ad 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,42 +1,42 @@
-import{i as h,b as k,d as v,a as y,c as P,r as b,e as r}from"./assets/header-5fcc9a4e.js";import{P as $}from"./assets/vendor-94a85c0e.js";function S({_id:t,list_name:e,author:o,title:n,book_image:s,description:g,buy_links:i}){return`<li class="shopping-item" data-id="${t}">
+import{i as b,b as k,d as S,a as $,c as v,r as L,e as i}from"./assets/header-5fcc9a4e.js";import{P}from"./assets/vendor-94a85c0e.js";function N({_id:e,list_name:t,author:a,title:s,book_image:o,description:n,buy_links:r}){return`<li class="shopping-item" data-id="${e}">
             <img
               class="shopping-img"
-              src="${s}"
-              alt="${n}"
+              src="${o}"
+              alt="${s}"
             />
             <div class="shopping-wrap">
               <div class="heading-info">
                 <div class="title-container">
                   <h2 class="title-book">
-                    ${n}
+                    ${s}
                   </h2>
-                  <p class="category-book">${e}</p>
+                  <p class="category-book">${t}</p>
                 </div>
                 <button
                   class="delete-shopping-item-btn"
                   
                 >
                   <svg class="trash-btn-icon" height="16" width="16">
-                    <use href="${h}#icon-trash"></use>
+                    <use href="${b}#icon-trash"></use>
                   </svg>
                 </button>
               </div>
-              <p class="descr-shopping">${g}</p>
+              <p class="descr-shopping">${n}</p>
               <div class="author-info-container">
-                <p class="author-info">${o}</p>
-                ${w(i)}
+                <p class="author-info">${a}</p>
+                ${E(r)}
               </div>
             </div>
-          </li>`}function w(t){const e=t.find(n=>n.name==="Amazon").url,o=t.find(n=>n.name==="Apple Books").url;return`<ul class="shop-link-list">
+          </li>`}function E(e){const t=e.find(s=>s.name==="Amazon").url,a=e.find(s=>s.name==="Apple Books").url;return`<ul class="shop-link-list">
               <li class=""shop-link-item>
-                <a class="shop-link-amazon" href="${e}" target="_blank">
-                   <img src="${k}" srcset="${v} 2x" alt="Amazon" class="amazon-image">
+                <a class="shop-link-amazon" href="${t}" target="_blank">
+                   <img src="${k}" srcset="${S} 2x" alt="Amazon" class="amazon-image">
                 </a>
               </li>
               <li class=""shop-link-item>
-                <a class="shop-link-amazon" href="${o}" target="_blank">
-                    <img src="${y}" srcset="${P} 2x" alt="Apple-books" class="apple-books-image">
+                <a class="shop-link-amazon" href="${a}" target="_blank">
+                    <img src="${$}" srcset="${v} 2x" alt="Apple-books" class="apple-books-image">
                 </a>
               </li>
-          </ul>`}function L(t,e){const o=document.querySelector("#tui-pagination-container");let n=2,s=4;window.innerWidth>=768&&(n=3,s=3);const g={totalItems:e.length,itemsPerPage:s,visiblePages:n,centerAlign:!0},i=new $(o,g);return i.on("afterMove",m=>{const c=m.page,d=c,u=e.slice((c-1)*s,c*s),f=b(S,u);t.innerHTML=f,i.prevPage=d}),i.movePageTo(i.getCurrentPage()),i}const p={isSupported(){return typeof Storage<"u"},has(t){return localStorage.hasOwnProperty(t)},get(t){let e=localStorage.getItem(t);if(typeof e!="string")return e;if(e!=="undefined")return e==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(e)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(e)?Number(e):e[0]==="{"||e[0]==="["?JSON.parse(e):e},set(t,e){if(typeof t!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${t}')`);(typeof e=="object"||Array.isArray(e))&&(e=JSON.stringify(e)),localStorage.setItem(t,e)},remove(t){localStorage.removeItem(t)}},l="cart";p.get(l);let a;function N(){r.shoppingListMain.innerHTML="";const t=p.get(l);!t||!t.length?r.shoppingEmpty.classList.remove("hidden"):(r.shoppingText.style.marginBottom="40px",r.shoppingEmpty.classList.add("hidden"),a=L(r.shoppingListMain,t),a.movePageTo(a.getCurrentPage()),document.querySelector(".js-shopping-main").addEventListener("click",T))}function T(t){if(t.target.tagName==="BUTTON"||t.target.tagName==="svg"||t.target.tagName==="use"){const e=t.target.closest(".shopping-item"),n=p.get(l).filter(({_id:s})=>s!==e.dataset.id);if(localStorage.setItem(l,JSON.stringify(n)),n.length/a._options.itemsPerPage<=1){a._view._containerElement.style.display="none";let s=a.getCurrentPage();a.reset(n.length),a.movePageTo(s)}e.remove()}}N();window.innerWidth<1440&&(document.querySelector(".aside").style.display="none");
+          </ul>`}const m={isSupported(){return typeof Storage<"u"},has(e){return localStorage.hasOwnProperty(e)},get(e){let t=localStorage.getItem(e);if(typeof t!="string")return t;if(t!=="undefined")return t==="null"?null:/^'-?\d{1,}?\.?\d{1,}'$/.test(t)||/^'-?\d{1}\.\d+e\+\d{2}'$/.test(t)?Number(t):t[0]==="{"||t[0]==="["?JSON.parse(t):t},set(e,t){if(typeof e!="string")throw new TypeError(`localStorage: Ключ повинен бути рядком. (зчитування '${e}')`);(typeof t=="object"||Array.isArray(t))&&(t=JSON.stringify(t)),localStorage.setItem(e,t)},remove(e){localStorage.removeItem(e)}};function d(e,t,a=1){const s=document.querySelector("#tui-pagination-container");let o=2,n=4;window.innerWidth>=768&&(o=3,n=3);const r={totalItems:t.length,itemsPerPage:n,visiblePages:o,centerAlign:!0};t.length/n<=1&&(s.style.display="none");const l=new P(s,r);return l.on("beforeMove",f=>{const g=f.page,u=t.slice((g-1)*n,g*n),y=L(N,u);e.innerHTML=y}),l.movePageTo(a),l}const p=document.querySelector(".js-shopping-main"),c="cart";let h;function w(){i.shoppingListMain.innerHTML="";const e=m.get(c);!e||!e.length?(i.shoppingEmpty.classList.remove("hidden"),p.style.display="none"):(i.shoppingText.style.marginBottom="40px",i.shoppingEmpty.classList.add("hidden"),h=d(i.shoppingListMain,e),p.addEventListener("click",z))}function z(e){if(e.target.tagName==="BUTTON"||e.target.tagName==="svg"||e.target.tagName==="use"){const t=m.get(c),a=e.target.closest(".shopping-item"),s=t.filter(({_id:r})=>r!==a.dataset.id);localStorage.setItem(c,JSON.stringify(s)),a.remove(),(!s||!s.length)&&(i.shoppingEmpty.classList.remove("hidden"),p.style.display="none");let n=h.getCurrentPage();d(i.shoppingListMain,s,n)}}w();
 //# sourceMappingURL=commonHelpers2.js.map
